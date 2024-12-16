439462cd9819f7abb2fc093314140be87bf78167
Gowtham <165543758+Gowthamguna01@users.noreply.github.com>
2024-12-15 17:49:07 -0800

    Update settings.py

--- Curdproject/Cproject/settings.py
+++ Curdproject/Cproject/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Cproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'yuvasmik',
         'USER': 'root',
         'PASSWORD': 'root',
